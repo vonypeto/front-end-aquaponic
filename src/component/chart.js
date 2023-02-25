5575c2bc5f7b4dc472597c2062bf8bb34cec3a01
Von Aralar <56251881+vonypeto@users.noreply.github.com>
2023-02-25 08:46:31 +0800

    Update chart.js

--- src/component/chart.js
+++ src/component/chart.js
@@ -95,8 +95,7 @@ const SplineChart = (props) => {
         </Tag>
       </div>
       <CanvasJSChart options={options} /* onRef={ref=> this.chart = ref} */ />
-      {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart
-                    properties and methods*/}
+     
     </div>
   );
 };
